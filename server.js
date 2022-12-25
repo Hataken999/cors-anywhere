e3577cc6dd6bd8dcbfffc308f3d9bc46a8989cdd
Hataken <75216370+Hataken999@users.noreply.github.com>
2022-12-25 17:51:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,39 +3,11 @@ const cors = require('cors-anywhere');
 
 const app = express();
 
-// Set whitelist for origins
-const whitelist = ['hataken999.github.io', '192.168.1.4:8080'];
+app.use(cors());
 
-// Set whitelist for IP addresses
-const whitelistIP = ['103.144.175.195'];
-
-// Set CORS options
-const corsOptions = {
-  origin: (origin, callback) => {
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if (whitelist.indexOf(origin) !== -1) {
-      if (whitelistIP.indexOf(ip) !== -1) {
-        callback(null, true);
-      }
-    } else {
-      callback(new Error('Your IP Address is not allowed to use this proxy!'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
-
-app.get('/', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  if (whitelistIP.indexOf(ip) !== -1) {
-    res.send('Selamat datang, Hataken999!');
-  } else {
-    res.send('Proxy hanya dapat digunakan oleh Hataken999!');
-  }
-});
-
-app.get('*', (req, res) => {
-  res.send('Route salah! Silahkan kembali ke main route.');
+app.get('/:url', (req, res) => {
+  const { url } = req.params;
+  cors.forward(req, res, url);
 });
 
 const port = process.env.PORT || 8080;
